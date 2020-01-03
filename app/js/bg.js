bfdbec1c3773936d0ef9421fa6514c0ca8a0ab85
schimbvalutar <hello@schimbvalutar.com>
2020-01-03 15:12:24 +0200

    Initial commit

---
+++ app/js/bg.js
@@ -0,0 +1,3 @@
+chrome.browserAction.onClicked.addListener(() => {
+    chrome.tabs.create({'url': 'https://schimbvalutar.com/#utm_source=ChromeNewTabApp&utm_medium=AppIcon&utm_campaign=ChromeNewTabApp.ver120'});
+});
